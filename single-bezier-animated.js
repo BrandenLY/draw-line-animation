82be4d6e694b0ace026568f4d1c7201c71da63f8
Branden Yarbrough <brandenly@gmail.com>
2025-03-18 17:26:19 -0700

    Update single-bezier-animated.js
    Configure canvas to resize appropriately and optimize different aspects of the script.

--- single-bezier-animated.js
+++ single-bezier-animated.js
@@ -1,12 +1,22 @@
+//
 // Variable Definitions
+//
+let prevWidth = Number(window.innerWidth);
+
 let c1 = null;
 let c2 = null;
 let a1 = null;
 let a2 = null;
-let lastDrawPoint = null;
-let bpoints = [];
-let drawnSegments = [];
 
+let bpoints = [];
+const pointCount = 120
+
+let drawnSegments = [];
+let lastDrawPoint = null;
+
+//
+// Helper Functions
+//
 function getStarting(){
     // The starting position must be off-screen.
     // RETURNS: 2D Vector Object
@@ -33,70 +43,106 @@ function getEnding(){
     return createVector(startingX, startingY);
 }
 
-// P5.js Functions
-function setup() {
+function repositionPoints(widthAdjustment){
+    
+    // Relocate end control point to screen edge
+    c2.add([widthAdjustment, 0, 0]);
+
+    // Relocate anchor points
+    const a1hpswr = a1.x / prevWidth; // a1 anchor point horizontal-position:screen-width ratio
+    const newA1XPos = windowWidth * a1hpswr; // calculate new horizontal position based on initial ratio
+    a1.x = newA1XPos
+
+    const a2hpswr = a2.x / prevWidth; // a1 anchor point horizontal-position:screen-width ratio
+    const newA2XPos = windowWidth * a2hpswr; // calculate new horizontal position based on initial ratio
+    a2.x = newA2XPos
+    
+} 
+
+//
+// P5.js Implementations
+//
+
+p5.disableFriendlyErrors = true; // Turns off the parts of the Friendly Error System (FES) that impact performance.
+
+function setup() { // A function that's called once when the sketch begins running.
+
+    const root = document.documentElement;
+    const rootStyles = getComputedStyle(root);
+    let primaryColor = rootStyles.getPropertyValue("--primary-c");
+    if(primaryColor === ""){
+        // Provide default color when style var isn't set.
+        primaryColor = "#5ebec4"
+    }
 
     // Canvas Configuration
     createCanvas(windowWidth, windowHeight - 4);
-    noStroke();
+    stroke(primaryColor);
+    strokeWeight(4);
     noFill();
 
-    // Bezier Curve Configuration
+    // Bezier Curve Initial Configuration
     c1 = getStarting(); // First Control Point
     c2 = getEnding(); // Second Control Point
     a1 = createVector( random(0, windowWidth), random(0, windowHeight) ); // First Anchor Point
     a2 = createVector( random(0, windowWidth), random(0, windowHeight) ); // Second Anchor Point
 
-    bezier(
-        c1['x'], c1['y'], // Control Point
-        a1['x'], a1['y'], // Anchor Point
-        a2['x'], a2['y'], // Anchor Point
-        c2['x'], c2['y']  // Control Point
-    );
-
-    // Locate points along the Bezier Curve
-    let steps = 120;
-    for (let i = 0; i <= steps; i++) {
-        let t = i / steps;
+    // Calculate 
+    for (let i = 0; i <= pointCount; i++) {
+        let t = i / pointCount;
         let x = bezierPoint(c1['x'], a1['x'], a2['x'], c2['x'], t);
         let y = bezierPoint(c1['y'], a1['y'], a2['y'], c2['y'], t);
         bpoints.push(createVector(x,y))
     }
-    bpoints.reverse();
-    strokeWeight(4);
-    const root = document.documentElement;
-    const rootStyles = getComputedStyle(root);
 
-    const primaryColor = rootStyles.getPropertyValue("--draw-line-animation-color");
-    stroke(primaryColor);
 }
 
-function draw() {
-    clear();
+function draw() { // A function that's called repeatedly while the sketch runs.
 
-    // Redraw: Any already-drawn lines.
-    drawnSegments.forEach(s => {
-        line(s[0]['x'], s[0]['y'], s[1]['x'], s[1]['y']);
-    });
+    // Initial animation is drawing
+    if (isLooping()){
 
-    //FIXME: Will try to draw a line between a point and null values.
-    if (lastDrawPoint) {
-        t1 = lastDrawPoint;
-    } else {
-        t1 = bpoints.pop();
+        if (lastDrawPoint) {
+            t1 = lastDrawPoint;
+        } 
+        else {
+            t1 = bpoints.shift();
+        }
+
+        t2 = bpoints.shift();
+    
+        if (t2 !== undefined){
+            line(t1['x'], t1['y'], t2['x'], t2['y']);
+            drawnSegments.push([t1,t2]);
+            lastDrawPoint = t2;
+        }
+        else {            
+            noLoop() // Stop Animation Loop
+        }
+
     }
-    t2 = bpoints.pop();
-
-    line(t1['x'], t1['y'], t2['x'], t2['y']);
-
-    drawnSegments.push([t1,t2]);
-    lastDrawPoint = t2;
+    else {
+        bezier(
+            c1['x'], c1['y'], // Control Point
+            a1['x'], a1['y'], // Anchor Point
+            a2['x'], a2['y'], // Anchor Point
+            c2['x'], c2['y']  // Control Point
+        );
+    }
 
 }
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight - 4);
-    redraw()
+function windowResized() { // A function that's called when the browser window is resized.
+
+    // Calculate difference in screenWidth
+    const screenWidthDifference = windowWidth - prevWidth;
+
+    // Trigger bezier redraw
+    resizeCanvas(windowWidth, windowHeight);
+    clear(); repositionPoints(screenWidthDifference); redraw();
+
+    prevWidth = Number(windowWidth) // Track any screen size changes
 }
+
 // Create a p5.js sketch
 new p5();
